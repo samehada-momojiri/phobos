7b1b96847bca8059c4ba491863acddd10e285cc2
WORLD\yusuk <yusuke.harada.007@gmail.com>
2026-04-21 21:57:32 +0900

    publishでエラーになる不具合の改修

--- src/main/java/com/nine2nine/lib/phobos/PhobosProvider.java
+++ src/main/java/com/nine2nine/lib/phobos/PhobosProvider.java
@@ -1,7 +1,15 @@
 package com.nine2nine.lib.phobos;
 
+/**
+ * "Hello, World!" メッセージを提供するシンプルなプロバイダーです。
+ */
 public class PhobosProvider {
 
+    /**
+     * 固定のメッセージを返します。
+     *
+     * @return {@code "Hello, World!"}
+     */
     public String getMessage() {
         return "Hello, World!";
     }
--- src/main/java/com/nine2nine/lib/phobos/autoconfigure/PhobosAutoConfiguration.java
+++ src/main/java/com/nine2nine/lib/phobos/autoconfigure/PhobosAutoConfiguration.java
@@ -5,9 +5,17 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * {@link PhobosProvider} を自動登録する AutoConfiguration です。
+ */
 @AutoConfiguration
 public class PhobosAutoConfiguration {
 
+    /**
+     * {@link PhobosProvider} が未定義の場合に Bean を登録します。
+     *
+     * @return {@link PhobosProvider} のインスタンス
+     */
     @Bean
     @ConditionalOnMissingBean
     public PhobosProvider phobosProvider() {
